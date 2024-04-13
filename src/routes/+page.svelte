2fd8600df6cb41d510d8c4ce9a8190b6f192e210
John Aitken <johna321123@gmail.com>
2024-04-12 22:20:10 -0400

    playing with svelte, a textbox component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import Textbox from './Textbox.svelte';
+import glab from '$lib/glab.png';
+import teaman from '$lib/teaman.png';
+let t = '';
+</script>
+
+<div class="flex flex-col">
+  <div class='p-4 mb-4'>
+    <h1 class='text-center mb-2 text-3xl font-bold'>glabophorb</h1>
+    <div>
+      <img class='max-w-xs mx-auto rounded-xl transition-all duration-300 filter grayscale hover:grayscale-0' src={teaman} alt="glabophorbs" />
+    </div>
+  </div>
+  <div class='mx-auto'>
+    <Textbox bind:text={t} />
+  </div>
+  <p class="mt-4 text-xl text-center">reactive update says you typed: </p>
+  <p class="text-center italic">{t}</p>
+</div>
+
---
+++ src/routes/Textbox.svelte
@@ -0,0 +1,9 @@
+<script>
+export let text = '';
+</script>
+
+<input
+  bind:value={text}
+  type="text"
+  class="bg-gray-50 border border-gray-300 text-gray-900 p-2 rounded"
+>
